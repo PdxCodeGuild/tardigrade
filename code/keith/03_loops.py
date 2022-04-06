13c9f67bc8da11fbfe1433df93c7275426210587
rabbitdub <rabbitdub13@gmail.com>
2022-04-06 13:05:28 -0700

    this is the second updated version of my loops lab

--- code/keith/03_loops.py
+++ code/keith/03_loops.py
@@ -6,41 +6,29 @@
 # Double Numbers
 # Write a function that takes a list of numbers and returns a new list with every number doubled
 
-# def double_numbers(nums):
-#     new_list = []
-#     for num in nums:
-#         new_list.append(num*2)
-#     return new_list
+def double_numbers(nums):
+    new_list = []
+    for num in nums:
+        new_list.append(num*2)
+    return new_list
 
-# def test_double_numbers():
-#     assert double_numbers([1, 2, 3]) == [2, 4, 6]
+def test_double_numbers():
+    assert double_numbers([1, 2, 3]) == [2, 4, 6]
 
-
+ 
 # Stars
 # Write a function that takes an integer and returns that number of asterisks in a string
 
 
-# numbers = int(input('ask!'))
-# nums = range(numbers)
-# lst = []
-# for number in nums:
-#     #print(number)
-#     lst.append('potato')
-# print(lst)
-
 
 def stars(n):
-     #for num in range(n)
+    my_string = ""
+    for x in range(0,n):
+        my_string += '*'
+    return my_string
 
-    # look into range and for loops
-    if n == 1:
-        return ('*')
-    elif n == 2:
-        return ('**')
-    elif n == 3:
-        return ('***')
-    elif n == 4:
-        return ('****')
+   
+   
 
 
 def test_stars():
@@ -48,19 +36,20 @@ def test_stars():
     assert stars(2) == '**'
     assert stars(3) == '***'
     assert stars(4) == '****'
+    
 
 
 # Extract Less Than Ten
 # Write a function to move all the elements of a list with value less than 10 to a new list and return it.
 
-# def extract_less_than_ten(nums):
-#     new_list = []
-#     for num in nums:
-#         if num < 10:
-#              new_list.append(num)
+def extract_less_than_ten(nums):
+    new_list = []
+    for num in nums:
+        if num < 10:
+             new_list.append(num)
     
-#     return new_list
+    return new_list
 
 
-# def test_extract_less_than_ten():
-#     assert extract_less_than_ten([2, 8, 12, 18]) == [2, 8]
+def test_extract_less_than_ten():
+    assert extract_less_than_ten([2, 8, 12, 18]) == [2, 8]
