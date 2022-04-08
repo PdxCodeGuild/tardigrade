c362cb91f608d88f1a97dce3ead3d95220ffcb45
deikner3 <69986521+deikner3@users.noreply.github.com>
2022-04-08 00:59:57 -0400

    modified the file as Version 2 (guess_the_number.py)

--- code/darrell/guess_the_number.py
+++ code/darrell/guess_the_number.py
@@ -4,13 +4,14 @@ print(random_number)
 
 
 x = 0
-while x < 10:
+while True:
     user_guess = input("Guess a number between 1 and 10: ")
     user_guess = int(user_guess)
-    if user_guess == random_number:
-        print(f"Congrats, you guessed the number! You guessed {x+1} times.")
-        break
-    else:
-        print("Please guess again!")
     x += 1
-print(f"Sorry, you're out of guesses! You guessed more than {x} times.")
+    if user_guess == random_number:
+        print(f"Congrats, you guessed the number! You guessed {x} times.")
+        break
+    # else:
+    #     print("Please guess again!")
+    #
+# print(f"Sorry, you're out of guesses! You guessed more than {x} times.")
