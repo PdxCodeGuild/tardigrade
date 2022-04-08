28e40f42d67c57dc424c6a50c2c1e9926af6be3c
unknown <joshuapitter96@gmail.com>
2022-04-07 22:32:15 -0600

    mad lib lab rename and guessing game

---
+++ code/joshua/Python/guessinggame.py
@@ -0,0 +1,30 @@
+import random
+
+
+def u_vs_cpu():
+    cpu_choice = random.randint(1,10)
+    user_tries = 10
+    user_choice = int(input('Hello\nPlease choose a number 1-10.... arigato...BTW you only have 10 tries hehehehhee\n'))
+    while True:
+        if user_tries == 1:
+            print('It took you every try just to fail... better luck next time')
+            break
+        
+        elif user_choice == cpu_choice:
+            print(f'Congrats!!!! You beat the computer\nYou had {user_tries} tries left. Good Job')
+            break
+        
+        elif user_choice < cpu_choice:
+            user_tries -= 1
+            print(f'Too Low, You were {abs(cpu_choice - user_choice)} away from the computer, try again please \n{user_tries} tries left')
+            
+            user_choice = int(input('Please choose a number 1-10\n'))
+            
+        
+        elif user_choice > cpu_choice:
+            user_tries -= 1
+            print(f'Too High, You were {abs(cpu_choice - user_choice)} away from the computer, try again please \n{user_tries} tries left')
+            
+            user_choice = int(input('Please choose a number 1-10\n'))
+
+u_vs_cpu()
