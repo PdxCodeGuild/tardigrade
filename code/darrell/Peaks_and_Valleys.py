a069c3bef4b0f19ef0f7f44531bd59b05e13cef8
deikner3 <69986521+deikner3@users.noreply.github.com>
2022-04-08 00:09:21 -0400

    fixed the Peaks_and_Valleys.py file

--- code/darrell/Peaks_and_Valleys.py
+++ code/darrell/Peaks_and_Valleys.py
@@ -3,10 +3,6 @@
 # valleys(data) - Returns the indices of 'valleys'. A valley is a number with a higher number on both the left and the right.
 # peaks_and_valleys(data) - uses the above two functions to compile a single list of the peaks and valleys in order of appearance in the original data.
 
-peak_list = []
-valley_list = []
-pv_list = []
-
 
 def peaks(data):
     x = 0
@@ -17,9 +13,7 @@ def peaks(data):
             peak_list.append(x)
             print(peak_list)
         x += 1
-    s = str(peak_list)
-    print(s)
-    return (s)
+    return peak_list
 
 
 def valleys(data):
@@ -31,19 +25,23 @@ def valleys(data):
             valley_list.append(x)
             print(valley_list)
         x += 1
-    return (valley_list)
+    return valley_list
 
 
 data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
-peaks(data)
-valleys(data)
+# peaks(data)
+# valleys(data)
 
 
 def peaks_and_valleys(data):
-    pv_list.append(peaks(data))
-    pv_list.append(valleys(data))
-    print(pv_list)
+    pv_list = []
+    peaks_list = peaks(data)
+    valleys_list = valleys(data)
+    for peak in peaks_list:
+        pv_list.append(peak)
+    for valley in valleys_list:
+        pv_list.append(valley)
     return pv_list
 
 
-peaks_and_valleys(data)
+print(peaks_and_valleys(data))
