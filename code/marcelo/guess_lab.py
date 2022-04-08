58faddb60f4c07fdc2ecc2bc72f40c068acfd547
Marcelo Alderete (Salt Lake City) <malderete@usanainc.com>
2022-04-08 12:29:04 -0600

    version2 of guess lab

--- code/marcelo/guess_lab.py
+++ code/marcelo/guess_lab.py
@@ -1,7 +1,7 @@
 import random
 
 computer= random.randint(1,10)
-trys= 1
+trys= 0
 while True:
     computer= random.randint(1,10)
     user= input('try to guess the right number: ')
@@ -10,9 +10,10 @@ while True:
     if user == computer:
         print(f"correct number {user} matches {computer}, it took you {trys} attempts")
         break
-    if user!=computer and trys < 10:
+    if user!=computer:
         trys += 1
         print(f'your number {user} is wrong. try again {computer}')
-    elif trys == 10:
-        print( "you are out of your 10 attempts. byeeeee")
-        break
+        print (f'you are curretly at {trys} attempts')
+    # elif trys == 10:
+    #     print( "you are out of your 10 attempts. byeeeee")
+    #     break
