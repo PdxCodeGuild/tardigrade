f57c1fc16a6a96c4002eaa7c6ade42580a6209a6
ShaneWaxwing <shane.d.waxwing@gmail.com>
2022-04-09 23:56:15 -0400

    added version 2 of guess the number ^_^

--- code/shane_waxwing/guess_the_number.py
+++ code/shane_waxwing/guess_the_number.py
@@ -2,16 +2,15 @@ import random
 guess_me = random.randint(0, 20)
 counter = 0
 
-while counter < 10:
+#--------------------------------------------------------------------------------#
+
+while True:
     guess = int(input("What number am I thinking of? Enter a number: "))
     if guess == guess_me:
-        print("You guessed it! You should probably go out and play the lottery.")
+        print(f"You guessed it!. It took you {counter} guess(es)")
         break
     else:
         print("Wrong. Try again: ")
-        print(guess_me)
         counter += 1
-        if counter == 10:
-            print("Game over, my friend...")
     
 
