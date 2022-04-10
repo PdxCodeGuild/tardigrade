51c3667c57eb8250d02e2d1e82948b5ff7cbcbf8
ShaneWaxwing <shane.d.waxwing@gmail.com>
2022-04-10 00:16:33 -0400

    Adding version 3 of Guess the Number

--- code/shane_waxwing/guess_the_number.py
+++ code/shane_waxwing/guess_the_number.py
@@ -1,5 +1,5 @@
 import random
-guess_me = random.randint(0, 20)
+guess_me = random.randint(0, 100)
 counter = 0
 
 #--------------------------------------------------------------------------------#
@@ -7,10 +7,15 @@ counter = 0
 while True:
     guess = int(input("What number am I thinking of? Enter a number: "))
     if guess == guess_me:
+        counter += 1
         print(f"You guessed it!. It took you {counter} guess(es)")
         break
-    else:
-        print("Wrong. Try again: ")
+    elif guess < guess_me:
         counter += 1
+        print(f"Number of Guesses: {counter}")
+        print("Too low! Try again!")
+    elif guess > guess_me:
+        counter += 1
+        print(f"Number of Guesses: {counter}")
+        print("Too high! Try again!")
     
-
