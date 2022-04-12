d151b8be501e80e789aff0a956d570e2a80cde95
Marcelo Alderete (Salt Lake City) <malderete@usanainc.com>
2022-04-11 21:35:59 -0600

    version 2 of units

--- code/marcelo/unitlab.py
+++ code/marcelo/unitlab.py
@@ -1,10 +1,17 @@
-unit_conversation_meter={
-    '1_ft': .3048
+unit_conversation_2meter={
+    'feet': .3048,
+    'mile' : 1609.34,
+    'm' : 1, 
+    'km': 1000 
 }
 
-user = input(' what is the distance in feet: ')
-user= int(user)
+distance= input('what is the distance: ')
+unit= input("what are the units.'feet, mile, m, km': ")
 
-total= user * unit_conversation_meter['1_ft']
+distance= int(distance)
 
-print (f' {user} ft is equal to {total} m')
+user_measurement= unit_conversation_2meter[unit]
+
+total= distance * user_measurement
+
+print (f' {distance} {unit} is equal to {total} m')
