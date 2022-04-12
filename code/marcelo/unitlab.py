24566255493abf642375dccce47ceacd7e57939e
Marcelo Alderete (Salt Lake City) <malderete@usanainc.com>
2022-04-11 22:46:12 -0600

    version 4 unit lab

--- code/marcelo/unitlab.py
+++ code/marcelo/unitlab.py
@@ -7,13 +7,23 @@ unit_conversation_2meter={
     'inch': .0254
 }
 
-distance= input('what is the distance: ')
-unit= input("what are the units.'feet, mile, m, km, yard, inch': ")
+print('you will be choosing 2 units to convert')
+distance= float(input('what is the distance: '))
+convert_from= input("what are the units.'feet, mile, m, km, yard, inch': ") #divide
+convert_to= input("what is the 2nd unit.'feet, mile, m, km, yard, inch': ") # multi
 
 distance= int(distance)
 
-user_measurement= unit_conversation_2meter[unit]
+unit_from= distance * unit_conversation_2meter[convert_from]
+print(unit_from)
 
-total= distance * user_measurement
+unit_to= unit_from / unit_conversation_2meter[convert_to]
+print(unit_to)
 
-print (f' {distance} {unit} is equal to {total} m')
+# user_measurement= unit_conversation_2meter[unit]
+# user_measurement2= unit_conversation_2meter[unit_2]
+
+# total= user_measurement * user_measurement2
+
+
+print (f'{distance} {convert_from} is {unit_to} {convert_to}')
