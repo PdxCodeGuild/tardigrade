bebfb58e0062de4a39629f599f894a35d8605a93
deikner3 <69986521+deikner3@users.noreply.github.com>
2022-04-11 23:59:07 -0400

    updated unit_converter.py to version 3

--- code/darrell/unit_converter.py
+++ code/darrell/unit_converter.py
@@ -1,11 +1,13 @@
 distance_in_meters = {"ft": 0.3048,
                       "mi": 1609.34,
                       "m": 1,
-                      "km": 1000}
+                      "km": 1000,
+                      "yd": 0.9144,
+                      "in": 0.0254}
 
 input_distance = input("Please enter distance to be converted: ")
 
-input_unit = input("Please enter the unit of measurement (ft/mi/m/km): ")
+input_unit = input("Please enter the unit of measurement (ft/mi/m/km/yd/in): ")
 
 distance_meters = int(input_distance) * distance_in_meters[input_unit]
 
