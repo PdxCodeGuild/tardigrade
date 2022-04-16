0abff362dddbd62713e28ff621494ba52bdc3c77
TSCSuser <info@tscs.tech>
2022-04-15 20:14:06 -0700

    UnitConverters.py

---
+++ code/gustavo/testing/unitconv.py
@@ -0,0 +1,9 @@
+units = {
+    'ft'    : 0.3048,
+    'm'     : 1,
+    'mi'    : 1609.34,
+    'km'    : 1000,
+}
+usr_meas = int(input('Please enter the number of feet you wish to convert: '))
+print(units['ft'] * usr_meas)
+
