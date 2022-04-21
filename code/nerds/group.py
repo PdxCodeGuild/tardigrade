485b7bccc7c3bf5f0f816f30abaa6da2d82ce4da
unknown <joshuapitter96@gmail.com>
2022-04-21 00:45:20 -0600

    test

--- code/nerds/group.py
+++ code/nerds/group.py
@@ -1,21 +1,21 @@
-# def legend_board(board):
+def legend_board(board):
 
     
-#     print('   |   |')
-#     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
-#     print('   |   |')
-#     print('-----------')
-#     print('   |   |')
-#     print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
-#     print('   |   |')
-#     print('-----------')
-#     print('   |   |')
-#     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
-#     print('   |   |')
+    print('   |   |')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('   |   |')
+    print('-----------')
+    print('   |   |')
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('   |   |')
 
-# test_board = [1,'1','2','3','4','5','6','7','8','9']
+test_board = [1,'1','2','3','4','5','6','7','8','9']
 
-# print(legend_board(test_board))
+print(legend_board(test_board))
 
 def display_board(board):
 
@@ -39,14 +39,14 @@ mark = 'X'
 
 if (board[7] == mark and board[8] == mark and board[9]== mark):
     print('WIN')
-    break
+
 elif (board[4] == mark and board[5] == mark and board[6]== mark):
     print('WIN')
-    break
-else:
-    continue 
 
-if player1 == turn:
-    mark = 'X'
-else:
-    mark = 'O'
+# else:
+#     continue 
+
+# if player1 == turn:
+#     mark = 'X'
+# else:
+#     mark = 'O'
