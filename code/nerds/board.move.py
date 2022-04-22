6d6a4eb1f915b3cc4cc36d1b0db155b316591103
TSCSuser <info@tscs.tech>
2022-04-21 20:58:33 -0700

    nerds

--- code/nerds/board.move.py
+++ code/nerds/board.move.py
@@ -1,7 +1,9 @@
 
 
-board = [' ',' ','2','3','4','5','6','7','8','9',] * 10
-mv_counter = 0
+
+
+board = [' ',' ','2','3','4','5','6','7','8','9',]
+
 
 class Player():
     def __init__(self, name, token):
@@ -31,22 +33,35 @@ Hello Players. \nWelcome to Nerds Tic Toe.
 ''')
 
 # input(f'Player 1, please choose your spot')
-def move(self, spot):
+def is_even(x):
+    if x % 2 == 0:
+        return True
+
+def move():
     p1 = 'X'
     p2 = 'O'
     spot = 0
-    
-    if mv_counter == 0:
-        player = p1
-        input(f"Hello Player 1 you are {player}'s. please choose your spot 1-9: ")
-        self.spot = board[spot].append(p1)
-    mv_counter += 1
-    print(self.spot)
-    
-    elif mv_counter == 1:
-        input(f"Hello Player 2 you are {player}'s. please choose your spot 1-9: ")
-        self.spot = board[spot].append(p2)
-    mv_counter += 1
-    print(self.spot)
+    mv_counter = 0
+    game = True
+    while game == True:
+        if is_even(mv_counter) == True:
+            player = p1
+            spot = int(input(f"Hello Player 1 you are {player}'s. please choose your spot 1-9: "))
+            board[spot] = player
 
+            mv_counter += 1
+            print(spot)
+            print(mv_counter)
+            print(board)
+    
+        else:
+            player = p2
+            spot = int(input(f"Hello Player 2 you are {player}'s. please choose your spot 1-9: "))
+            board[spot] = player
+            mv_counter += 1
+            print(mv_counter)
+            print(board)
+    return board
+    print(spot)
+print(move())
 print(board)
