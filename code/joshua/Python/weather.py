da5a9a4847e0b6f9418e67f51f34fb10e794d4c6
unknown <joshuapitter96@gmail.com>
2022-05-08 19:21:16 -0600

    html bio labl

--- code/joshua/Python/weather.py
+++ code/joshua/Python/weather.py
@@ -1,9 +1,10 @@
 
 import requests
+import json
 API_key = '35b725d260988e4245ae3e67ec6b8f3d'
 
-user_zip = input('What is your zip code?\n')
-response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?zip={user_zip}&appid={API_key}&units=imperial")
+# user_zip = input('What is your zip code?\n')
+response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?zip={11413}&appid={API_key}&units=imperial")
 
 data = response.json()
 
@@ -19,4 +20,5 @@ Your location is {data['name']}
 {data['name']}'s temp: {data['main']['temp']} in F
 {data['name']}'s outside condition: {data['weather'][0]['description']}
                         """
-print(user_return)
+# print(user_return)
+print(json.dumps(data,indent = 2))
