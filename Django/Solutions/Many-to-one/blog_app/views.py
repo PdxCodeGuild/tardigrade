c7b9370dd129c5c64ea2eb11e6775aa71f71e9b4
alexdemichieli <demichieli.alessandro@gmail.com>
2022-05-23 20:11:14 -0700

    added changes to many to one

--- Django/Solutions/Many-to-one/blog_app/views.py
+++ Django/Solutions/Many-to-one/blog_app/views.py
@@ -2,6 +2,8 @@ from tkinter import E
 from django.shortcuts import render,redirect
 from .models import Reporter, Article
 
+## https://docs.djangoproject.com/en/4.0/topics/db/examples/many_to_one/
+
 def add_reporter(request):
     if request.method=="GET":
         return render(request, 'home.html')
@@ -19,20 +21,14 @@ def add_article(request):
     elif request.method == "POST":
         headline = request.POST['headline']
         pub_date= request.POST['pub_date']
-        reporter = Reporter.objects.get(id=request.POST['reporter'])
+        reporter = Reporter.objects.get(id=request.POST['reporter']) ##Queries the database and saves an instance of the reporter object in the variable reporter
         Article.objects.create(headline=headline, pub_date=pub_date, reporter=reporter)
-        # Article.objects.create(headline="This is a test", pub_date=date(2005, 7, 27), reporter=r3)
         print(reporter,pub_date,headline)
         return redirect('add_article')
  
 
 def view_all(request):
-    #Article.objects.filter(reporter__pk=1) ##pk is primary key
-    #Reporter.objects.filter(article__pk=1)
-    all_reporters = Reporter.objects.all()
-    all_articles = Article.objects.all()
-    context = {
-        "all_reporters": all_reporters,
-        "all_articles":all_articles
-    }
-    return render(request, 'list.html', context)
+    #Article.objects.filter(reporter__pk=1) ##pk is primary key. This gives a list of articles linke to the reporter with ID of 1
+    #Reporter.objects.filter(article__pk=1) Gives a list of reporters linked to an article
+    all_reporters = Reporter.objects.all()   
+    return render(request, 'list.html', {"all_reporters":all_reporters})
