a31a82fbea3292447ebd254cf6c095ea80278f5b
Christian <Christian.L.Eberhardt@gmail.com>
2022-07-05 22:24:19 -0600

    Alex & Will - Here is the code with serveral improvements. I'll begin working CAPSTONE from here on out.

--- code/christian/Django/todo_list_codemy/todo_list/models.py
+++ code/christian/Django/todo_list_codemy/todo_list/models.py
@@ -4,9 +4,11 @@ from django.db import models
 
 class List(models.Model):
     item = models.CharField(max_length=200)
+    date_created = models.DateField(auto_now_add=True, auto_now=False)
+    date_completed = models.DateField(null=True, blank=True)
     completed = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.item + ' | ' + str(self.completed)
+        return self.item + ' | ' + str(self.completed) + ' | ' + str(self.date_created)+ ' | ' + str(self.date_completed)
     
         
